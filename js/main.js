ccd6597145497eddd412fa5e02814d6573d28ce2
kennedy mutethia <kenmutesh901@gmail.com>
2023-01-18 00:30:24 +0300

    modes

--- js/main.js
+++ js/main.js
@@ -1,30 +1,7 @@
-console.clear();
+const toggle = document.querySelector('.toggle-input');
+const initialState = localStorage.getItem('toggleState') == 'true';
+toggle.checked = initialState;
 
-const typing = document.querySelectorAll('.typewriter');
-
-function type(element) {
-
-function randomOpacity() {
-  return (Math.floor(Math.random() * 50) + 50)/100;
-}
-
-function randomEms() {
-  if (Math.random() > .8) {
-  return (Math.floor(Math.random() * 100) - 50)/800;
-  }
-  else {
-    return 0;
-  }
-}
-
-function wrap(char) {
-  return '<span style="opacity:' + randomOpacity() + '; text-shadow:' + randomEms() + 'em ' + randomEms() + 'em currentColor;">' + char + '</span>';
-}
-
-const wrappedText = Array.from(element.textContent).map(wrap);
-
-element.innerHTML = wrappedText.join('');
-  
-}
-
-typing.forEach(type);
+toggle.addEventListener('change', function() {
+  localStorage.setItem('toggleState', toggle.checked);
+});
