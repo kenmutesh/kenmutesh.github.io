048bb6ffaf75db8437369de252c2d89501628631
kennedy mutethia <kenmutesh901@gmail.com>
2023-01-21 23:44:21 +0300

    set side styles

---
+++ js/switch.js
@@ -0,0 +1,4 @@
+const styleSwitcherToggle =  document.querySelector(".style-switcher-toggler");
+styleSwitcherToggle.addEventListener('click', ()=>{
+    document.querySelector(".style-switcher").classList.toggle("open");
+})
