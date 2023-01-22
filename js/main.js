287e26110eea18ed25ab13c688d1ad89e9f08bc5
kennedy mutethia <kenmutesh901@gmail.com>
2023-01-22 11:27:37 +0300

    add styles day night toggle

--- js/main.js
+++ js/main.js
@@ -5,35 +5,12 @@ const typing = new Typed(".typing",{
     backspeed: 60,
 })
 
-var particleArray = [];
-// Set the number of particles
-var particleCount = 350;
-// Create a function to generate the particles
-function generateParticles() {
-  for (var i = 0; i < particleCount; i++) {
-    var particle = document.createElement("div");
-    particle.className = "particle";
-    // Set the particle's initial position
-    particle.style.left = Math.random() * 100 + "%";
-    particle.style.top = Math.random() * 100 + "%";
-    // Add the particle to the array and the HTML
-    particleArray.push(particle);
-    document.getElementById("body").appendChild(particle);
-  }
-}
-// Call the function to generate the particles
-generateParticles();
-// Create a function to animate the particles
-function animateParticles() {
-    for (var i = 0; i < particleArray.length; i++) {
-      var particle = particleArray[i];
-      // Generate new x and y positions for the particle
-      var newX = particle.offsetLeft + (Math.random() - 0.5) * 20;
-      var newY = particle.offsetTop + (Math.random() - 0.5) * 20;
-      // Update the particle's position
-      particle.style.left = newX + "px";
-      particle.style.top = newY + "px";
-    }
-  }
-  // Animate the particles every 50 milliseconds
-  setInterval(animateParticles, 150);
+const active = document.querySelectorAll(".navbar");
+active.forEach(item => {
+    item.addEventListener("click", (event)=>{
+        // Remove the 'active' class from all <li> elements
+    active.forEach(item => item.classList.remove("active"));
+    // Add the 'active' class to the clicked <li>
+    event.target.classList.add("active");
+    });
+})
